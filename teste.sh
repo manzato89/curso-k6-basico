d64b7421aa901114a0c34741379b21d52308a4ad
Anderson Manzato <manzato89@gmail.com>
2024-07-29 00:13:19 -0300

    Primeiro Teste com K6

--- teste.sh
+++ teste.sh
@@ -1,4 +1,4 @@
 curl --request POST \
 --url http://localhost:3333/signup \
 --header 'Content-Type: application/json' \
---data '{"email": "anderson@example.com", "password": "pwd456"}'
+--data '{"email": "manzato@example.com", "password": "pwd123"}'
