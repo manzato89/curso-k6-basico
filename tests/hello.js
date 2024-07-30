6e52150b333bcbf983aa008351780b228da1c013
Anderson Manzato <manzato89@gmail.com>
2024-07-29 22:08:50 -0300

    Validando status com Code Check

--- tests/hello.js
+++ tests/hello.js
@@ -1,10 +1,13 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
-export const options = {
-  vus: 10,
-  duration: '30s',
-};
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+
+
 export default function () {
-  http.get('http://localhost:3333');
-  sleep(1);
+  const res = http.get('http://localhost:3333');
+
+  check(res, {
+    'status should be 200': (r) => r.status === 200
+  })
+
+  sleep(1)
 }
